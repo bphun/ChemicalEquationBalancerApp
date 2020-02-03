d2722669a5374b4eafad1f043928e2de4f746673
Brandon Phan <brandon.phan.73@gmail.com>
2020-02-03 10:21:38 -0800

    Add docker image updater and nginx web server

---
+++ iOS/ChemicalEquationBalancerApp/Environment.swift
@@ -0,0 +1,9 @@
+//
+//  Environment.swift
+//  ChemicalEquationBalancerApp
+//
+//  Created by Brandon Phan on 1/27/20.
+//  Copyright © 2020 Brandon Phan. All rights reserved.
+//
+
+import Foundation
