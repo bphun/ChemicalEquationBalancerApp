b9ce9005b6b06b5b27cb96a4c70bcb87d78289c6
Brandon Phan <brandon.phan.73@gmail.com>
2020-02-21 23:33:10 -0800

    Add spring actuator endpoints for prometheus scraping

--- api/src/main/java/com/bphan/ChemicalEquationBalancerAppServer/ChemicalEquationBalancerAppServerApplication.java
+++ api/src/main/java/com/bphan/ChemicalEquationBalancerAppServer/ChemicalEquationBalancerAppServerApplication.java
@@ -3,11 +3,20 @@ package com.bphan.ChemicalEquationBalancerAppServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.micrometer.prometheus.PrometheusConfig;
+import io.micrometer.prometheus.PrometheusMeterRegistry;
+
 @SpringBootApplication
 public class ChemicalEquationBalancerAppServerApplication {
 
-	public static void main(String[] args) {
+    PrometheusMeterRegistry prometheusRegistry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
+
+    public static void main(String[] args) {
 		SpringApplication.run(ChemicalEquationBalancerAppServerApplication.class, args);
     }
 
+    // @Bean
+    // InitializingBean forcePrometheusPostProcessor(BeanPostProcessor meterRegistryPostProcessor, PrometheusMeterRegistry registry) {
+    //     return () -> meterRegistryPostProcessor.postProcessAfterInitialization(registry, "");
+    // }
 }
