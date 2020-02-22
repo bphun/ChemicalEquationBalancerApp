422647127927ed7e35a515cc7f5fe6783e54fff8
Brandon Phan <brandon.phan.73@gmail.com>
2020-02-21 23:37:09 -0800

    Add Grafana dashboard and prometheus server

---
+++ grafana/buildDockerImage.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker build -t bphun/chemical-equation-balancer:grafana .
+
+
---
+++ prometheus/buildDockerImage.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker build -t bphun/chemical-equation-balancer:prometheus .
+#&& \
+#docker push bphun/chemical-equation-balancer:prometheus
