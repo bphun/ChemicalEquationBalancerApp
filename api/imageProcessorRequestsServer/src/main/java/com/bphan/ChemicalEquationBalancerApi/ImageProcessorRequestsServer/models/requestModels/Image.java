2838fc5847be81b5bb1144f525f43eb235d5309b
Brandon Phan <brandon.phan.73@gmail.com>
2020-03-06 19:41:33 -0800

    Oops

---
+++ api/imageProcessorRequestsServer/src/main/java/com/bphan/ChemicalEquationBalancerApi/ImageProcessorRequestsServer/models/requestModels/Image.java
@@ -0,0 +1,13 @@
+package com.bphan.ChemicalEquationBalancerApi.ImageProcessorRequestsServer.models.requestModels;
+
+public class Image {
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String value) {
+        this.content = value;
+    }
+}
---
+++ api/imageProcessorRequestsServer/src/main/java/com/bphan/ChemicalEquationBalancerApi/ImageProcessorRequestsServer/models/storedRequestInfoModels/RequestLabelingStatus.java
@@ -0,0 +1,19 @@
+package com.bphan.ChemicalEquationBalancerApi.ImageProcessorRequestsServer.models.storedRequestInfoModels;
+
+public enum RequestLabelingStatus {
+    INCOMPLETE("INCOMPLETE"), IN_PROGRESS("IN_PROGRESS"), LABELED("LABELED"), TEST_STATUS("TEST_STATUS");
+
+    private String status;
+
+    private RequestLabelingStatus(String status) {
+        this.status = status;
+    }
+
+    public String toStr() {
+        return this.status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
